e4b459e1cd55d9f94eb24fe72d2107f1487055cf
shivhek25 <shivamsinghal0610@gmail.com>
2020-06-13 19:03:55 +0530

    Added a cube to check anti-aliasing

--- main.js
+++ main.js
@@ -1,21 +1,44 @@
 
+//Scene and Camera
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-var renderer = new THREE.WebGLRenderer();
+
+//Renderer with Anti-Aliasing
+var renderer = new THREE.WebGLRenderer({antialias : true});
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
+//Renderer without Anti-Aliasing
+var renderer1 = new THREE.WebGLRenderer();
+renderer1.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer1.domElement);
+
+//Cube of white color
 var geometry = new THREE.BoxGeometry();
 var material = new THREE.MeshBasicMaterial({color : 0xffffff});
-var cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+
+//Cube 1
+var cube1 = new THREE.Mesh(geometry, material);
+cube1.position.setX(-1);
+scene.add(cube1);
+
+//Cube2
+var cube2 = new THREE.Mesh(geometry, material);
+cube2.position.setX(1);
+scene.add(cube2);
+
 camera.position.z = 3;
 
 function animate(){
 	requestAnimationFrame(animate);
 	renderer.render(scene, camera);
-	cube.rotation.x += 0.05;
-	cube.rotation.y += 0.05;
+	renderer1.render(scene, camera);
+	//Rotating Cube 1
+	cube1.rotation.x += 0.02;
+	cube1.rotation.y += 0.02;
+	//Rotating Cube 2
+	cube2.rotation.x += 0.02;
+	cube2.rotation.y += 0.02;
 }
 
 animate();
